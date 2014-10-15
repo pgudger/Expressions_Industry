07982c0f9b3d76b2667fd9f609cbabb3874651e1
Gudger_Parvel_WPF <plgudger@fullsail.edu>
2014-10-15 03:46:04 -0400

    Expressions_Industry

---
+++ js/script.js
@@ -0,0 +1,27 @@
+//Industry_Specific
+alert(hoursWorked);
+
+Hours worked = 68;
+var hoursWorked = 68;
+
+monday = 16;
+var monday = 16;
+
+tuesday = 16;
+var tuesday = 16;
+
+wednesday = 10;
+var wednesday = 10;
+
+thursday = 16;
+var thursday = 16;
+
+friday = 10;
+var friday = 10;
+
+console.log("Hours worked");
+Console.log(Hours worked);
+console.log("Days and hours worked for days monday 16 tuesday 16 wednesday 10 thursday 16 friday 10");
+
+
+
